333e1d4f8eb941a4dc57dd225a0cfec1b2c0df55
HongDaeEui <day100@enterblock.co>
2022-01-19 17:14:31 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../App.css';
+import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
+import {useWeb3React} from "@web3-react/core"
+import {injected} from "../components/wallet/connectors"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+    const {active, account, library, connector, activate, deactivate} = useWeb3React()
+
+    async function connect() {
+        try {
+            await activate(injected)
+        } catch (ex) {
+            console.log(ex)
+        }
+    }
+
+    async function disconnect() {
+        try {
+            deactivate()
+        } catch (ex) {
+            console.log(ex)
+        }
+
+        return (
+            <div className="App flex flex-col items-center justify-center">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Connecting MetaMask with React
+                    </p>
+                    <button
+                        className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800">Connect
+                        to MetaMask
+                    </button>
+                    <span>Not connected</span>
+                    <button
+                        className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800">Disconnect
+                    </button>
+                </header>
+            </div>
+        );
+    }
+
+    export default App;
